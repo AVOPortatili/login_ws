4603ce85bfed6e73d319c6299355dbb14169f887
AVOPortatili <168416502+AVOPortatili@users.noreply.github.com>
2024-06-03 11:13:34 +0200

    aggiunto supporto mail, creazione utenti e possibilità di cambiare password.

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import routes from './routes/routes.js';
 import bodyParser from 'body-parser'
+import dotenv from "dotenv";
 import cors from 'cors'
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.listen(8080); //todo: metti in env
+app.listen(process.env.PORT);
 app.use(routes);
 app.use((req,res)=> {
     res.status(404).send("404: route not found")
